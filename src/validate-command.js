c50e54b2a2b89475cd742832efd051df998b1e2a
Renato Rodrigues <ren.gsg@gmail.com>
2021-04-30 09:01:11 -0300

    andamento

--- src/validate-command.js
+++ src/validate-command.js
@@ -1,1 +1,4 @@
-export const VL_COMMAND_INITIALIZE_SERVER = /^\$md config$/
+export const VL_COMMAND_INITIALIZE_SERVER = /^\$md (config|cfg)$/
+export const VL_COMMAND_LIST_VARS = /^\$md (list|ls)$/
+export const VL_COMMAND_GET_VAR = /^\$md get [a-zA-Z]+$/
+// export const VL_COMMAND_ROLLING = /(\d)/
