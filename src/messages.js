d2ea359fcb67ac88016707ca1503feccec716a3c
Renato Rodrigues <ren.gsg@gmail.com>
2021-04-30 15:05:47 -0300

    correção das mensagens

--- src/messages.js
+++ src/messages.js
@@ -1,12 +1,12 @@
-export const MESSAGE_START_CONFIGURE = `I\'m feeling welcome here 😊!\nI'll start with the basic configuration...`
-export const MESSAGE_CREATE_MEMORY_CHANNEL = `I'm creating a channel to organize my memory 📝🤯.`
-export const MESSAGE_END_CONFIGURE = `All right now! 👌\nI will listen to your orders from that channel. 💂‍♂️`
-export const MESSAGE_NOT_CONFIGURED_YET = `The MemoDice is not configured yet! 😥\nUse the command '$md config' in channel command receiver for start configure.`
-export const MESSAGE_UNEXPECTED_ERROR = `Something unexpected error happens! 😣`
-export const MESSAGE_PROTECT_MEMORY = `Master, never use that channel.\nIt can disrupt my memory. 😵`
-export const MESSAGE_USER_NOT_MEMO = `I'm sorry, but you have not yet stored a rolling. ⌛🎲\nTry send to me this '$md --attack=1d20'`
-export const MESSAGE_MANY_ROLLINGS = `Thank God 😅!\nThere were a lot of dices but I finished!`
-export const MESSAGE_VAR_NOTFOUND = `I\'m sorry master 😥. I didn\'t find that scroll.`
-export const MESSAGE_CRITICAL_ROLLING = ` - Wow! How lucky you are! Beautiful Critic!🍀👏`
-export const MESSAGE_FAIL_ROLLING = ` - 💣 You were unlucky in that! Maybe it will improve next time.`
-export const MESSAGE_LARGE_ROLLING = ` - My God! How much more dice will I have to roll. 😅`
+export const MESSAGE_START_CONFIGURE = `\nI\'m feeling welcome here 😊!\nI'll start with the basic configuration...`
+export const MESSAGE_CREATE_MEMORY_CHANNEL = `\nI'm creating a channel to organize my memory 📝🤯.`
+export const MESSAGE_END_CONFIGURE = `\nAll right now! 👌\nI will listen to your orders from that channel. 💂‍♂️`
+export const MESSAGE_NOT_CONFIGURED_YET = `\nThe MemoDice is not configured yet! 😥\nUse the command '$md config' in channel command receiver for start configure.`
+export const MESSAGE_UNEXPECTED_ERROR = `\nSomething unexpected error happens! 😣`
+export const MESSAGE_PROTECT_MEMORY = `\nMaster, never use that channel.\nIt can disrupt my memory. 😵`
+export const MESSAGE_USER_NOT_MEMO = `\nI'm sorry, but you have not yet stored a rolling. ⌛🎲\nTry send to me this '$md --attack=1d20'`
+export const MESSAGE_MANY_ROLLINGS = `\nThank God 😅!\nThere were a lot of dices but I finished!`
+export const MESSAGE_VAR_NOTFOUND = `\nI\'m sorry master 😥. I didn\'t find that scroll.`
+export const MESSAGE_CRITICAL_ROLLING = `\n - Wow! How lucky you are! Beautiful Critic!🍀👏`
+export const MESSAGE_FAIL_ROLLING = `\n - 💣 You were unlucky in that! Maybe it will improve next time.`
+export const MESSAGE_LARGE_ROLLING = `\n - My God! How much more dice will I have to roll. 😅`
