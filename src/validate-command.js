44c04b966de4f58663069540e0c57191ef4235db
Renato Rodrigues <ren.gsg@gmail.com>
2021-05-03 12:27:17 -0300

    proteção

--- src/validate-command.js
+++ src/validate-command.js
@@ -1,4 +1,4 @@
 export const VL_COMMAND_INITIALIZE_SERVER = /^\$md (config|cfg)$/
 export const VL_COMMAND_LIST_VARS = /^\$md (list|ls)$/
 export const VL_COMMAND_GET_VAR = /^\$md get [a-zA-Z]+$/
-export const VL_COMMAND_ROLLING = /^(((\d+d\d+|\d+)\s*(\+|\-)\s*)*(\d+d\d+)(\s*(\+|\-)\s*(\d+d\d+|\d+))*)$/i
+export const VL_COMMAND_ROLLING = /^(((\d{1,3}d\d{1,4}|\d{1,4})\s*(\+|\-)\s*)*(\d{1,3}d\d{1,4})(\s*(\+|\-)\s*(\d{1,3}d\d{1,4}|\d{1,4}))*)$/i
